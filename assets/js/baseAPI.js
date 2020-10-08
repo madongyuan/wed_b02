651d24dd0c19b2f76fd4155e18fd9b1124139ca4
yuaotian <ma115200@yu>
2020-10-08 20:53:11 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,17 @@
 var qwd = 'http://ajax.frontend.itheima.net'
 $.ajaxPrefilter(function (base) {
-    base.url = qwd  + base.url
+    base.url = qwd + base.url
+    if (base.url.indexOf('/my/') !== -1) {
+        base.headers = {
+           Authorization :localStorage.getItem('token') || ''
+        }
+    }
+    base.complete = function (res) {
+        console.log(res.responseJSON);
+        var obj = res.responseJSON
+        if (obj.status == 1 && obj.message == '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
